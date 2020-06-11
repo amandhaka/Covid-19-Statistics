36f2bc0f7cecea38cb282cfdf2fff33977f61bcf
amandhaka <amandhaka2021@gmail.com>
2020-06-11 20:37:36 +0530

    Updates

---
+++ precache-manifest.8a66f16720013a2daf794b52b5dafa69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfef581777765e843a468237fe827f15",
+    "url": "/Covid-19-Statistics/index.html"
+  },
+  {
+    "revision": "f666f9a77ace34cb01fb",
+    "url": "/Covid-19-Statistics/static/css/main.261ea0a1.chunk.css"
+  },
+  {
+    "revision": "5182bc82439cc3614901",
+    "url": "/Covid-19-Statistics/static/js/2.2d45c6db.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Statistics/static/js/2.2d45c6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f666f9a77ace34cb01fb",
+    "url": "/Covid-19-Statistics/static/js/main.c83c7275.chunk.js"
+  },
+  {
+    "revision": "f949b5f604081f8fe1af",
+    "url": "/Covid-19-Statistics/static/js/runtime-main.7a07f2a0.js"
+  }
+]);
